4441606575fd46c36122a9cc2764e424fc3569ba
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2016-03-30 17:01:11 +0200

    Grace should also O_TRUNC

--- Grace.c
+++ Grace.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define MAKE_LINE char*s="#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#define MAKE_LINE char*s=%c%s%c;%c#define GET_FILE int fd = open(%cGrace_kid.c%c, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);if (fd == -1){return (1);}%c%c#define FT int main(void){MAKE_LINE GET_FILE dprintf(fd,s,10,10,10,34,s,34,10,34,34,10,10,10,10,10);}%c%cFT%c";
-#define GET_FILE int fd = open("Grace_kid.c", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);if (fd == -1){return (1);}
+#define MAKE_LINE char*s="#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#define MAKE_LINE char*s=%c%s%c;%c#define GET_FILE int fd = open(%cGrace_kid.c%c, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);if (fd == -1){return (1);}%c%c#define FT int main(void){MAKE_LINE GET_FILE dprintf(fd,s,10,10,10,34,s,34,10,34,34,10,10,10,10,10);}%c%cFT%c";
+#define GET_FILE int fd = open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);if (fd == -1){return (1);}
 
 #define FT int main(void){MAKE_LINE GET_FILE dprintf(fd,s,10,10,10,34,s,34,10,34,34,10,10,10,10,10);}
 
